484b0576d1ae436fbe1d40153a7a2de1ea6d5666
Jonathan Law <jon_law98@hotmail.com>
2025-01-02 21:38:50 +0800

    Added logo section

--- components/HomeSectionDatingAI.vue
+++ components/HomeSectionDatingAI.vue
@@ -1,14 +1,26 @@
 <template>
-<v-container class="pa-16">
-  <v-row>
-    <v-col>
-      <h3 class="text-right" style="font-size: 2rem">And the worst part...<span class="random-rotate">?</span></h3>
-    </v-col>
-    <v-col>
-      <h4 class="text-left" style="font-size: 4rem">AI does not make it easier</h4>
-    </v-col>
-  </v-row>
-</v-container>
+  <v-container class="pa-16">
+    <v-row>
+      <v-col>
+        <h3 class="text-right" style="font-size: 2rem">And the worst part...<span class="random-rotate">?</span></h3>
+      </v-col>
+      <v-col>
+        <h4 class="text-left" style="font-size: 4rem">AI does not make it easier</h4>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="text-center">
+        <v-img min-width="240px" max-width="720px" src="/img/cards.webp" width="100%"></v-img>
+      </v-col>
+      <v-col>
+        <br>
+        <p style="font-size: 1.5rem; max-width: 720px;">What if...</p>
+        <p style="font-size: 1.5rem; max-width: 720px;">I can use AI to mimic the people around me, to <span
+            class="font-italic">manipulate</span> the perception of me, to show common interest, to be... <span
+            class="font-weight-bold font-italic">not me</span>.</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
